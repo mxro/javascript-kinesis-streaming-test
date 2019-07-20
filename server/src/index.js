97ad1f1581951ce61d6bfa3980d5a5c4dedc383b
Max Rohde <mrohde@whispir.com>
2019-07-20 13:52:49 +1000

    Trying to access stream on server

--- server/src/index.js
+++ server/src/index.js
@@ -2,28 +2,32 @@ const express = require('express');
 const Peer = require('simple-peer');
 const wrtc = require('wrtc');
 const bodyParser = require('body-parser');
+const { RTCVideoSink, RTCVideoSource, i420ToRgba, rgbaToI420 } = require('wrtc').nonstandard;
 
 const app = express();
 
 const port = 3000;
 
-let offer;
+let signals = [];
 
 const peer1 = new Peer({ initiator: true, wrtc: wrtc });
 
 peer1.on('signal', data => {
   console.log('SIGNAL', JSON.stringify(data));
-  if (data.type === 'offer') {
-    offer = JSON.stringify(data);
-  }
+  signals.push(data);
 });
 
 peer1.on('connect', () => {
   console.log('CONNECT');
 });
 
-peer1.on('stream', stream => {
+peer1.on('stream', mediaStream => {
   console.log('receiving stream');
+  console.log(mediaStream.getAudioTracks());
+  console.log(mediaStream.getVideoTracks());
+  console.log(peer1._pc);
+  console.log(peer1._pc.getReceivers());
+  console.log(mediaStream.getVideoTracks()[0].id);
 });
 
 peer1.on('track', function (track, stream) {
@@ -36,15 +40,16 @@ peer1.on('data', function (chunk) {
 
 app.use(bodyParser.json());
 
-app.get('/offer', (req, res) => res.send(offer));
+app.get('/signals', (req, res) => {
+  res.send(JSON.stringify(signals));
+  signals = [];
+});
 
-app.post('/response', (req, res) => {
-  console.log(req.body);
+app.post('/receiveSignal', (req, res) => {
+  console.log('recieveSignmal', req.body);
   peer1.signal(req.body);
   res.send('success');
 });
-
-
 
 app.use(express.static('../client'));
 
